e6424499789bf481dcb16cbf8a89d4d1ce3dd714
Geethika Nagasri Tammineni <tamminenigeethika9@gmail.com>
2026-01-06 08:58:35 +0530

    Add variables and string methods practice

---
+++ Variables.js
@@ -0,0 +1,25 @@
+// Purpose: Practice JavaScript variables, data types, and string methods
+
+// Numbers
+const age = 20;
+const currentYear = 2026;
+
+// Strings
+const firstName = "Geethika";
+const lastName = "Nagasri";
+const fullName = `${firstName} ${lastName}`;
+
+// Boolean
+const isLearningJavaScript = true;
+
+// Console output
+console.log("Age:", age);
+console.log("Year:", currentYear);
+console.log("Full Name:", fullName);
+console.log("Learning JavaScript:", isLearningJavaScript);
+
+// String methods
+console.log("Name Length:", fullName.length);
+console.log("Uppercase:", fullName.toUpperCase());
+console.log("First 5 Characters:", fullName.substring(0, 5));
+console.log("Characters Array:", fullName.split(""));
